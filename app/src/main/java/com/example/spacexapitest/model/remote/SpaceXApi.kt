5bc38f6e3a3a4185b23b8a4f37664ba97dd0686f
devch89 <carl.howard@enhanceit.us>
2022-11-07 11:42:37 -0500

    implemented Response type endpoint

--- app/src/main/java/com/example/spacexapitest/model/remote/SpaceXApi.kt
+++ app/src/main/java/com/example/spacexapitest/model/remote/SpaceXApi.kt
@@ -1,5 +1,6 @@
 package com.example.spacexapitest.model.remote
 
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
@@ -8,7 +9,7 @@ interface SpaceXApi {
     @POST(ROUTES_LAUNCHES)
     suspend fun getAllLaunches(): LaunchResponse
     @GET(ROUTES_COMPANAY)
-    suspend fun getCompanyInfo(): CompanyResponse
+    suspend fun getCompanyInfo(): Response<CompanyResponse>
     @GET(ROUTES_ROCKET)
     suspend fun getRocketInfo(
         @Path(ROCKET_ID) rocketId: String
